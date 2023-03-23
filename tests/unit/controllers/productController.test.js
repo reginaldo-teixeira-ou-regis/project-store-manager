aef4b15855e3ef27579667a5480e4c0c227ee6b3
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-03-22 22:33:41 -0300

    Requisite 02 complete and refactored (Done 100% of the findAll and findById function tests in all layers of the application)

--- tests/unit/controllers/productController.test.js
+++ tests/unit/controllers/productController.test.js
@@ -25,17 +25,43 @@ describe('Unit test of the productController', function () {
       expect(res.status).to.have.been.calledWith(200);
       expect(res.json).to.have.been.calledWith(productList);
     });
+
+    it("Returns an ERROR when not finding the product list", async function () {
+      const res = {};
+      const req = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(productService, "findAll")
+        .resolves({
+          type: "PRODUCT_NOT_FOUND",
+          message: "Product not found",
+        });
+
+      await productController.findAll(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: "Product not found" });
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
   });
 
   describe("Searching for a product by id", function () {
     it("Must respond with status 200 and the database data when it exists", async function () {
       const res = {};
-      const req = { params: { id: 1 } };
+      const req = {
+        params: { id: 1 },
+      };
 
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
+
       sinon.stub(productService, "findById")
-        .resolves(productList[0]);
+        .resolves({ type: null, message: productList[0] });
 
       await productController.findById(req, res);
 
@@ -45,22 +71,44 @@ describe('Unit test of the productController', function () {
 
     it("When passing an invalid id it should return an error", async function () {
       const res = {};
-      const req = { params: { id: "abc" } };
+      const req = {
+        params: { id: "abc" },
+      };
 
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
 
-      sinon.stub(productService, "findById").resolves([[productList[0]]]);
-        try {
-          await productController.findById("abc");
-        } catch (error) {
-          expect(error.status).to.have.been.calledWith(200);
-          expect(error.json).to.have.been.calledWith({
-              status: 404,
-              message: 'Product not found',
-            });
-        }
+      sinon.stub(productService, "findById")
+        .resolves({ type: "INVALID_VALUE", message: '"id" must be a number' });
+
+      await productController.findById(req, res);
+
+      expect(res.status).to.have.been.calledWith(422);
+      expect(res.json).to.have.been.calledWith({
+          message: '"id" must be a number',
+        });
       });
+
+    it("When passing an id that does not exist in the database, an error should be returned", async function () {
+      const res = {};
+      const req = {
+        params: { id: 1111111 },
+      };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(productService, "findById")
+        .resolves({
+          type: "PRODUCT_NOT_FOUND",
+          message: "Product not found",
+        });
+
+      await productController.findById(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: "Product not found" });
+    });
 
     afterEach(function () {
       sinon.restore();
--- tests/unit/services/productService.test.js
+++ tests/unit/services/productService.test.js
@@ -17,19 +17,23 @@ describe("Checking product service", function () {
 
   describe("Search for a product by id", function () {
     it("Returns an error if it receives an invalid ID", async function () {
+      const result = await productService.findById("a");
+      expect(result.type).to.equal("INVALID_VALUE");
+      expect(result.message).to.equal('"id" must be a number');
+    });
+
+    it("Returns an error if the product does not exist", async function () {
       sinon.stub(productModel, "findById").resolves(undefined);
-      try {
-        await productService.findById("abc");
-      } catch (error) {
-        expect(error.status).to.equal(404);
-        expect(error.message).to.equal('Product not found');
-      }
+      const result = await productService.findById(1);
+      expect(result.type).to.equal("PRODUCT_NOT_FOUND");
+      expect(result.message).to.equal("Product not found");
     });
 
     it("Returns the product if the ID exists", async function () {
       sinon.stub(productModel, "findById").resolves(productList[0]);
       const result = await productService.findById(1);
-      expect(result).to.deep.equal(productList[0]);
+      expect(result.type).to.equal(null);
+      expect(result.message).to.deep.equal(productList[0]);
     });
 
     afterEach(function () {
